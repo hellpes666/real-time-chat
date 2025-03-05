68cbece0779e79e60e7dfdeec4f35daafbc621d3
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-03-05 16:35:03 +0300

    change cond in required fields

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -5,12 +5,19 @@ import User from "@models/user.model";
 import { z } from "zod";
 
 const signupSchema = z.object({
-	firstName: z.string().min(1, "Имя обязательно"),
-	lastName: z.string().min(1, "Фамилия обязательна"),
+	firstName: z
+		.string()
+		.min(1, "Имя обязательно")
+		.max(128, "Имя не может быть длиньше 36 символов"),
+	lastName: z
+		.string()
+		.min(1, "Фамилия обязательна")
+		.max(128, "Фамилия не может быть длиньше 36 символов"),
 	email: z.string().email("Некорректный email"),
 	password: z
 		.string()
 		.min(6, "Пароль должен содержать минимум 6 символов")
+		.max(128, "Пароль не может содержать более 128 символов")
 		.regex(/[a-zA-Z]/, "Пароль должен содержать хотя бы одну букву")
 		.regex(/\d/, "Пароль должен содержать хотя бы одну цифру")
 		.regex(
@@ -52,11 +59,9 @@ export const signup = async (req: Request, res: Response) => {
 		});
 	} catch (error) {
 		if (error instanceof z.ZodError) {
-			return res
-				.status(400)
-				.json({
-					message: error.errors.map((e) => e.message).join(", "),
-				});
+			return res.status(400).json({
+				message: error.errors.map((e) => e.message).join(", "),
+			});
 		}
 		console.error("Ошибка в signup controller:", error);
 		res.status(500).json({ message: "Ошибка сервера." });
